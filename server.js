5fd88d4842505ca0452740104bf02f8aece3b754
nikirosinar <nrotar@provectus.com>
2019-01-02 17:10:05 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');    //Express Web Server 
+var busboy = require('connect-busboy'); //middleware for form/file upload
+var path = require('path');     //used for file path
+var fs = require('fs-extra');       //File System - for file manipulation
+
+var app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+app.use(busboy());
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+/* ========================================================== 
+Create a Route (/upload) to handle the Form submission 
+(handle POST requests to /upload)
+Express v4  Route definition
+============================================================ */
+
+app.route('/upload')
+    .post(function (req, res, next) {
+        console.log('request')
+
+        var fstream;
+        req.pipe(req.busboy);
+        req.busboy.on('file', function (fieldname, file, filename) {
+            console.log("Uploading: " + filename);
+
+            //Path where image will be uploaded
+            fstream = fs.createWriteStream(__dirname + '/img/' + filename);
+            file.pipe(fstream);
+            fstream.on('close', function () {
+                console.log("Upload Finished of " + filename);
+                res.end();
+            });
+        });
+    });
+
+var server = app.listen(3333, function() {
+    console.log('Listening on port %d', server.address().port);
+});
